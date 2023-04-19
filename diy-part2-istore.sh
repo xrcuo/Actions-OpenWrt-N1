e394a550565bc191e9e87186322897d5eea06e4e
xrcuo <102892174+xrcuo@users.noreply.github.com>
2023-04-19 21:59:57 +0800

    Actions-OpenWrt-N1

--- diy-part2-istore.sh
+++ diy-part2-istore.sh
@@ -13,3 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.24/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/small-package
+
+sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/sxrcuo/Actions-OpenWrt-N1#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i 's#ARMv8#openwrt_armvirt_v8#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
