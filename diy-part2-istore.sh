56763ea899a7b4b4eada84c10be7e42af584272c
xrcuo <102892174+xrcuo@users.noreply.github.com>
2023-04-20 11:38:16 +0800

    OpenWrt

--- diy-part2-istore.sh
+++ diy-part2-istore.sh
@@ -14,6 +14,6 @@
 sed -i 's/192.168.1.1/192.168.2.24/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/small-package
 
-sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/sxrcuo/Actions-OpenWrt-N1#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
-sed -i 's#ARMv8#openwrt_armvirt_v8#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/xrcuo/Actions-OpenWrt-N1#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i 's#ARMv8#openwrt_armvirt_iStore#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
