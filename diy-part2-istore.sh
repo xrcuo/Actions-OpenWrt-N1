a8e641e13238e33a4f1b1f6d7bf74f284da28134
xrcuo <102892174+xrcuo@users.noreply.github.com>
2023-04-20 13:13:15 +0800

    amlogic

--- diy-part2-istore.sh
+++ diy-part2-istore.sh
@@ -13,4 +13,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.24/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/small-package
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
+rm -rf package/small-package/luci-app-amlogic
+
+sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/xrcuo/Actions-OpenWrt-N1#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i 's#ARMv8#openwrt_armvirt_iStore#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+ 
