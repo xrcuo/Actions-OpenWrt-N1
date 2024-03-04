0af776d01d6962df7ec4b762198d7611c5fb11b9
xrcuo <836447856@qq.com>
2024-03-04 08:45:52 +0000

    s

--- diy-part2-istore.sh
+++ diy-part2-istore.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.24/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.24/g' package/base-files/files/bin/config_generate
 
 # 拉取软件包
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon
-git clone https://github.com/kenzok8/small-package package/small-package
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/kenzok8/small-package package/small-package
 
 # 删除重复包
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf package/small-package/luci-app-amlogic
-rm -rf package/small-package/luci-theme-argon*
+rm -rf package/small-package/luci-app-openvpn-server
+rm -rf package/small-package/openvpn-easy-rsa-whisky
+rm -rf package/small-package/luci-app-wrtbwmon
+rm -rf package/small-package/wrtbwmon
+rm -rf package/small-package/luci-app-koolproxyR
+rm -rf package/small-package/luci-app-godproxy
 rm -rf package/small-package/luci-app-argon*
-rm -rf package/small-package/luci-app-argon-config*
-rm -rf package/small-package/luci-app-argone-config*
+rm -rf package/small-package/luci-theme-argon*
+rm -rf package/small-package/luci-app-amlogic
+rm -rf package/small-package/luci-app-unblockneteasemusic
+rm -rf package/small-package/upx-static
+rm -rf package/small-package/upx
+rm -rf package/small-package/firewall*
+rm -rf package/small-package/opkg
+rm -rf package/feeds/packages/aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+rm -rf package/feeds/packages/perl-xml-parser
+rm -rf package/feeds/packages/xfsprogs
 
 # 其他调整
 sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/xrcuo/Actions-OpenWrt-N1#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
